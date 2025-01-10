c79c638058f9a8360aa424afae9f1e2eeb8c0cad
EricNaKor <122855875+EricNakor@users.noreply.github.com>
2025-01-10 18:12:50 +0900

    feat: 수 찾기

---
+++ src/algorythm202501_1/problem1920.java
@@ -0,0 +1,40 @@
+package algorythm202501_1;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class problem1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            bw.write(linearSearch(A, target) ? "1\n" : "0\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static boolean linearSearch(int[] arr, int target) {
+        for (int num : arr) {
+            if (num == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
