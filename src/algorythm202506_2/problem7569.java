171bdd52c1f96eca92519d3524fd4be40ddfc2ab
EricNa_Kor <ericna130@gmail.com>
2025-06-15 19:24:59 +0900

    feat: 7569. 토마토

--- src/algorythm202506_2/problem7569.java
+++ src/algorythm202506_2/problem7569.java
@@ -3,6 +3,34 @@ package algorythm202506_2;
 import java.io.*;
 import java.util.*;
 
+//public class problem7569 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int M = Integer.parseInt(st.nextToken());
+//        int N = Integer.parseInt(st.nextToken());
+//        int H = Integer.parseInt(st.nextToken());
+//
+//        int[][][] box = new int[H][N][M];
+//        Queue<int[]> queue = new LinkedList<>();
+//        int unripeCount = 0;
+//
+//        for (int h = 0; h < H; h++) {
+//            for (int n = 0; n < N; n++) {
+//                st = new StringTokenizer(br.readLine());
+//                for (int m = 0; m < M; m++) {
+//                    box[h][n][m] = Integer.parseInt(st.nextToken());
+//                    if (box[h][n][m] == 1) {
+//                        queue.offer(new int[]{h, n, m}); // 익은 토마토 위치를 큐에 추가
+//                    } else if (box[h][n][m] == 0) {
+//                        unripeCount++; // 익지 않은 토마토 개수 카운트
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+
 public class problem7569 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
