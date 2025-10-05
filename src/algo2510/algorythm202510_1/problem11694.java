400114e3e4fda23767ac63dfe858b76a8d4615ce
EricNa_Kor <ericna130@gmail.com>
2025-10-05 19:06:49 +0900

    feat: 다시 풀기

---
+++ src/algo2510/algorythm202510_1/problem11694.java
@@ -0,0 +1,28 @@
+package algo2510.algorythm202510_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class problem11694 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int nimSum = 0;
+        for (int i = 0; i < N; i++) {
+            int pile = Integer.parseInt(st.nextToken());
+            nimSum ^= pile; // XOR 연산을 누적
+        }
+
+        // 님 합이 0이 아니면 선공 필승
+        if (nimSum != 0) {
+            System.out.println("koosaga");
+        } else { // 님 합이 0이면 후공 필승
+            System.out.println("cubelover");
+        }
+    }
+}
