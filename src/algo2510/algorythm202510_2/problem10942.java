13a48ffc607506b2acf698f4ee4bb48948765088
EricNa_Kor <ericna130@gmail.com>
2025-10-12 00:33:36 +0900

    feat: 기하학, 문자열, DP, Greedy

---
+++ src/algo2510/algorythm202510_2/problem10942.java
@@ -0,0 +1,57 @@
+package algo2510.algorythm202510_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class problem10942 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean[][] dp = new boolean[N + 1][N + 1];
+
+        // 길이가 1인 부분 수열
+        for (int i = 1; i <= N; i++) {
+            dp[i][i] = true;
+        }
+
+        // 길이가 2인 부분 수열
+        for (int i = 1; i <= N - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                dp[i][i + 1] = true;
+            }
+        }
+
+        // 길이가 3 이상인 부분 수열
+        for (int len = 3; len <= N; len++) {
+            for (int start = 1; start <= N - len + 1; start++) {
+                int end = start + len - 1;
+                if (arr[start] == arr[end] && dp[start + 1][end - 1]) {
+                    dp[start][end] = true;
+                }
+            }
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            if (dp[S][E]) {
+                sb.append("1\n");
+            } else {
+                sb.append("0\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
