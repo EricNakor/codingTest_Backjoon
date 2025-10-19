4a31424ceba5eb39001496d2b5d6d84316974582
EricNa_Kor <ericna130@gmail.com>
2025-10-19 17:51:10 +0900

    feat: 기하학, 문자열, DP, Greedy

---
+++ src/algo2510/algorythm202510_3/problem2504.java
@@ -0,0 +1,55 @@
+package algo2510.algorythm202510_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class problem2504 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+        int result = 0;
+        int temp = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c == '(') {
+                temp *= 2;
+                stack.push(c);
+            } else if (c == '[') {
+                temp *= 3;
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    result = 0;
+                    break;
+                }
+                if (str.charAt(i - 1) == '(') {
+                    result += temp;
+                }
+                stack.pop();
+                temp /= 2;
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    result = 0;
+                    break;
+                }
+                if (str.charAt(i - 1) == '[') {
+                    result += temp;
+                }
+                stack.pop();
+                temp /= 3;
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            result = 0;
+        }
+
+        System.out.println(result);
+    }
+}
